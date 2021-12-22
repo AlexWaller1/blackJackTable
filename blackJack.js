75ca45f4a8f8b38f234fd6992ace46654f422633
Alex Waller <alex@artofalexwaller.com>
2021-12-22 01:40:51 -0500

    fullDeck

--- blackJack.js
+++ blackJack.js
@@ -1,22 +1,289 @@
+console.log("A Gambler's Anatomy");
+
 // blackJack Table
 
 // Beginning of BlackJack Game
 // Just do what you can for right now.
 
-const jack = 10;
-const queen = 10;
-const king = 10;
-let ace = "";
+// number cards equal their number
+// Jack, Kings, and Queens all equal 10
+// an Ace can equal 1 or 11, it's the player's choice
 
-let clubs = [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king];
+const startGameBtn = document.getElementById("start-button");
 
-let diamonds = [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king];
+const hearts = [
+  {
+    name: "2 of Hearts",
+    value: 2,
+    image: ""
+  },
+  {
+    name: "3 of Hearts",
+    value: 3,
+    image: ""
+  },
+  {
+    name: "4 of Hearts",
+    value: 4,
+    image: ""
+  },
+  {
+    name: "5 of Hearts",
+    value: 5,
+    image: ""
+  },
+  {
+    name: "6 of Hearts",
+    value: 6,
+    image: ""
+  },
+  {
+    name: "7 of Hearts",
+    value: 7,
+    image: ""
+  },
+  {
+    name: "8 of Hearts",
+    value: 8,
+    image: ""
+  },
+  {
+    name: "9 of Hearts",
+    value: 9,
+    image: ""
+  },
+  {
+    name: "10 of Hearts",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Jack of Hearts",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Queen of Hearts",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "King of Hearts",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Ace of Hearts",
+    value: "",
+    image: ""
+  }
+];
 
-let hearts = [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king];
+const spades = [
+  {
+    name: "2 of Spades",
+    value: 2,
+    image: ""
+  },
+  {
+    name: "3 of Spades",
+    value: 3,
+    image: ""
+  },
+  {
+    name: "4 of Spades",
+    value: 4,
+    image: ""
+  },
+  {
+    name: "5 of Spades",
+    value: 5,
+    image: ""
+  },
+  {
+    name: "6 of Spades",
+    value: 6,
+    image: ""
+  },
+  {
+    name: "7 of Spades",
+    value: 7,
+    image: ""
+  },
+  {
+    name: "8 of Spades",
+    value: 8,
+    image: ""
+  },
+  {
+    name: "9 of Spades",
+    value: 9,
+    image: ""
+  },
+  {
+    name: "10 of Spades",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Jack of Spades",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Queen of Spades",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "King of Spades",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Ace of Spades",
+    value: "",
+    image: ""
+  }
+];
 
-let spades = [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king];
+const clubs = [
+  {
+    name: "2 of Clubs",
+    value: 2,
+    image: ""
+  },
+  {
+    name: "3 of Clubs",
+    value: 3,
+    image: ""
+  },
+  {
+    name: "4 of Clubs",
+    value: 4,
+    image: ""
+  },
+  {
+    name: "5 of Clubs",
+    value: 5,
+    image: ""
+  },
+  {
+    name: "6 of Clubs",
+    value: 6,
+    image: ""
+  },
+  {
+    name: "7 of Clubs",
+    value: 7,
+    image: ""
+  },
+  {
+    name: "8 of Clubs",
+    value: 8,
+    image: ""
+  },
+  {
+    name: "9 of Clubs",
+    value: 9,
+    image: ""
+  },
+  {
+    name: "10 of Clubs",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Jack of Clubs",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Queen of Clubs",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "King of Clubs",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Ace of Clubs",
+    value: "",
+    image: ""
+  }
+];
 
-const fullDeck = [...clubs, ...diamonds, ...hearts, ...spades];
+const diamonds = [
+  {
+    name: "2 of Diamonds",
+    value: 2,
+    image: ""
+  },
+  {
+    name: "3 of Diamonds",
+    value: 3,
+    image: ""
+  },
+  {
+    name: "4 of Diamonds",
+    value: 4,
+    image: ""
+  },
+  {
+    name: "5 of Diamonds",
+    value: 5,
+    image: ""
+  },
+  {
+    name: "6 of Diamonds",
+    value: 6,
+    image: ""
+  },
+  {
+    name: "7 of Diamonds",
+    value: 7,
+    image: ""
+  },
+  {
+    name: "8 of Diamonds",
+    value: 8,
+    image: ""
+  },
+  {
+    name: "9 of Diamonds",
+    value: 9,
+    image: ""
+  },
+  {
+    name: "10 of Diamonds",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Jack of Diamonds",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Queen of Diamonds",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "King of Diamonds",
+    value: 10,
+    image: ""
+  },
+  {
+    name: "Ace of Diamonds",
+    value: "",
+    image: ""
+  }
+];
+
+const fullDeck = [...hearts, ...spades, ...clubs, ...hearts];
 
 let count = 0;
 let moneyBet = 0;
